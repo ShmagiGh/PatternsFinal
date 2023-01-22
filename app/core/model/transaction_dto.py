6de39df5513835599d7d652040406472bb727818
bachiSkhulukhia <bskhu18@freeuni.edu.ge>
2023-01-22 18:19:44 +0400

    fix typo

--- app/core/model/transaction_dto.py
+++ app/core/model/transaction_dto.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 @dataclass
 class TransactionDTO:
     amount: float
-    commision: float
+    commission: float
     coin_type_id: int
     wallet_from_address: str
     wallet_to_address: str
