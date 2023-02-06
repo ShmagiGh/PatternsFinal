43caa2856496ece1613ddc9145ef5d044b5aa9c7
bachiSkhulukhia <bskhu18@freeuni.edu.ge>
2023-02-06 22:48:16 +0400

    change transaction_repo typos

--- app/infra/sqlite/transaction_repo.py
+++ app/infra/sqlite/transaction_repo.py
@@ -46,66 +46,66 @@ class TransactionRepository(ITransactionRepository):
         )
         self.db.conn.commit()
 
-        def create_transaction(self, transaction: TransactionDTO) -> None:
-            self.db.cur.execute(
-                """INSERT INTO transactions (amount,
-                                             commision,
-                                             coin_type_id,
-                                             wallet_from_address,
-                                             wallet_to_address)
-                                VALUES(?,?,?,?,?)""",
-                (
-                    transaction.amount,
-                    transaction.commission,
-                    transaction.coin_type_id,
-                    transaction.wallet_from_address,
-                    transaction.wallet_to_address,
+    def create_transaction(self, transaction: TransactionDTO) -> None:
+        self.db.cur.execute(
+            """INSERT INTO transactions (amount,
+                                         commision,
+                                         coin_type_id,
+                                         wallet_from_address,
+                                         wallet_to_address)
+                            VALUES(?,?,?,?,?)""",
+            (
+                transaction.amount,
+                transaction.commission,
+                transaction.coin_type_id,
+                transaction.wallet_from_address,
+                transaction.wallet_to_address,
+            ),
+        )
+        self.db.conn.commit()
+
+    def get_transactions_of_user(self, api_key: str) -> List[TransactionDTO]:
+        wallets = self.wallet_repo.get_wallets(api_key=api_key)
+        transactions_list = list(
+            map(
+                lambda wallet: self._get_transactions(
+                    address=wallet.address, address_type=AddressType.to_wallet
                 ),
+                wallets,
             )
-            self.db.conn.commit()
+        )
+        flat_list = [item for sublist in transactions_list for item in sublist]
+        return flat_list
 
-        def get_transactions_of_user(self, api_key: str) -> List[TransactionDTO]:
-            wallets = wallet_repo.get_wallets(api_key=api_key)
-            transactions_list = list(
-                map(
-                    lambda wallet: _get_transactions(
-                        address=wallet.address, address_type=AddressType.to_wallet
-                    ),
-                    wallets,
-                )
-            )
-            flat_list = [item for sublist in transactions_list for item in sublist]
-            return flat_list
+    def get_transactions_sent(self, address: str) -> List[TransactionDTO]:
+        self._get_transaction(address=address, address_type=AddressType.to_wallet)
 
-        def get_transactions_sent(self, address: str) -> List[TransactionDTO]:
-            self._get_transaction(address=address, address_type=AddressType.to_wallet)
+    def get_transactions_received(self, address: str) -> List[TransactionDTO]:
+        self._get_transaction(address=address, address_type=AddressType.from_wallet)
 
-        def get_transactions_received(self, address: str) -> List[TransactionDTO]:
-            self._get_transaction(address=address, address_type=AddressType.from_wallet)
+    def _get_transactions(
+        self, address: str, address_type: AddressType
+    ) -> List[TransactionDTO]:
+        transactions = self.db.cur.execute(
+            """SELECT (amount,
+                       commision,
+                       coin_type_id,
+                       wallet_from_address,
+                       wallet_to_address)
+                 FROM transactions
+                 WHERE ? = ?
+            """,
+            (address_type.value, address),
+        ).fetchall()
 
-        def _get_transactions(
-            self, address: str, address_type: AddressType
-        ) -> List[TransactionDTO]:
-            transactions = self.db.cur.execute(
-                """SELECT (amount,
-                           commision,
-                           coin_type_id,
-                           wallet_from_address,
-                           wallet_to_address)
-                     FROM transactions
-                     WHERE ? = ?
-                """,
-                (address_type.value, address),
-            ).fetchall()
-
-            transactions = map(
-                lambda elem: TransactionDTO(
-                    amount=elem[0],
-                    commission=elem[1],
-                    coin_type_id=elem[2],
-                    wallet_from_address=elem[3],
-                    wallet_to_address=elem[4],
-                ),
-                transactions,
-            )
-            return transactions
+        transactions = map(
+            lambda elem: TransactionDTO(
+                amount=elem[0],
+                commission=elem[1],
+                coin_type_id=elem[2],
+                wallet_from_address=elem[3],
+                wallet_to_address=elem[4],
+            ),
+            transactions,
+        )
+        return transactions
