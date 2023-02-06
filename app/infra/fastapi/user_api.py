1b9fea9417ed1065b2e794288806aee1346e013e
Dimitri Adeishvili <Dimitri_Adeishvili@epam.com>
2023-02-07 00:33:13 +0400

    package changes of get_core method

--- app/infra/fastapi/user_api.py
+++ app/infra/fastapi/user_api.py
@@ -1,19 +1,14 @@
 from fastapi import APIRouter, Depends
-from starlette.requests import Request
 
 from app.core import BitcoinWalletCore
 from app.core.model.user_dto import UserDTO
+from app.infra.fastapi import get_core
 
 user_api = APIRouter()
 
 
-def get_core(request: Request) -> BitcoinWalletCore:
-    core: BitcoinWalletCore = request.app.state.core
-    return core
-
-
 @user_api.post("/users", status_code=201)
 def register_user(
-    first_name: str, last_name: str, core: BitcoinWalletCore = Depends(get_core)
+        first_name: str, last_name: str, core: BitcoinWalletCore = Depends(get_core)
 ) -> UserDTO:
     return core.userInterface.create_user(user=UserDTO(first_name, last_name))
